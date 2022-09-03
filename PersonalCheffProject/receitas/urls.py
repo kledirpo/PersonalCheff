54e7b956a0230be4ceb8f17aec7f54092323bd3f
Kledir Oliveira <kledir@protonmail.com>
2022-09-03 14:02:44 -0300

    Adição de receitas

--- PersonalCheffProject/receitas/urls.py
+++ PersonalCheffProject/receitas/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('receitas/', views.receitas, name='receitas'),
+    path('sucodelaranja', views.sucodelaranja, name='sucodelaranja')
+    path('miojo', views.miojo, name='miojo')
 
 ]
--- PersonalCheffProject/receitas/views.py
+++ PersonalCheffProject/receitas/views.py
@@ -6,3 +6,9 @@ def index(request):
 def receitas(request):
     return render(request, 'index.html')
 
+def sucodelaranja(request):
+    return render(request, 'sucodelaranja.html')
+
+def miojo(request):
+    return render(request, 'miojo.html')
+
